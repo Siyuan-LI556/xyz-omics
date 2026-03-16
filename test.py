3d17592f85b0bb82958534495c7d14ef65f73f4e
Benjamin Charlier <benjamin.charlier@inrae.fr>
2026-03-16 16:01:00 +0100

    Upgrade first attempt of varifold approx. Still WIP.

--- test.py
+++ test.py
@@ -1,8 +1,24 @@
+#%%
+import os.path
+
 import numpy as np
 import torch
 import torch.optim as optim
 import pyvista as pv
 
+
+device = torch.device("cuda" if torch.cuda.is_available() * 0. else "cpu")
+print(f"Using device: {device}")
+
+
+#%% Set Paths
+data_dir = os.path.join("..", "data", "BARSeq")
+output_dir = os.path.join(data_dir, "output")
+os.makedirs(output_dir, exist_ok=True)
+
+
+
+#%% Declare kernel functions and varifold loss
 
 def gaussian_kernel(x, y, sigma):
     dist_sq = torch.cdist(x, y, p=2)**2
@@ -19,33 +35,43 @@ def varifold_sp(S1, S2, sigma=1.0):
     # Normalize by the product of the number of points to prevent extremely large Loss values
     return torch.sum(K_pos * K_feat) / (x1.shape[0] * x2.shape[0])
 
-
+#%% Load data
 print("Loading .npz file...")
-#data = np.load("AllenAtlas_approx200um_flipZ.npz")
-data = np.load("D076_1L_approx200um.npz")
+data_dir = os.path.join("..", "data", "BARSeq")
 
-# Extract spatial coordinates (62453, 3) and gene features (62453, 39)
-#X_np = data['Z']
+data = np.load(os.path.join(data_dir, "D076_1L_approx200um.npz"))
+
+# Barseq data contains 62,453 points with 39 gene features each
+Position_BarSeq = data['X'] # a.ka. x_i in xIV-LDDMM paper
+Feature_BarSeq = data['nu_X']  #  a.ka. w_i*p_i in xIV-LDDMM paper, total gene expression per cell
+
+print(f"Loaded data with {Position_BarSeq.shape[0]} points and {Feature_BarSeq.shape[1]} gene features.")
+print(f"Feature range is {Feature_BarSeq.min()}, {Feature_BarSeq.max()}")
+
+# Allen atlas.
+#data = np.load("AllenAtlas_approx200um_flipZ.npz")
+#Z_np = data['Z']
 #P_np = data['nu_Z']
 
-X_np = data['X']
-P_np = data['nu_X']
-
 # Convert to PyTorch tensors
-X_orig = torch.tensor(X_np, dtype=torch.float32)
-P_orig = torch.tensor(P_np, dtype=torch.float32)
+X_orig = torch.tensor(Position_BarSeq, dtype=torch.float32, device=device)
+P_orig = torch.tensor(Feature_BarSeq, dtype=torch.float32, device=device)
 
+#%% Normalize spatial coordinates to [0, 1] range
 X_min, _ = torch.min(X_orig, dim=0)
 X_max, _ = torch.max(X_orig, dim=0)
 X_orig = (X_orig - X_min) / (X_max - X_min)
 S = (X_orig, P_orig)
 
+bandwith_varifold = 0.10 # TODO : check dependency to M
 
+
+#%% Subsample data
 N = X_orig.shape[0]  
-M = 2000             
-
+M = 2000
 print(f"Preparing to compress {N} brain cell points into {M} representative points...")
 
+# TODO: Implement more sophisticated subsampling strategies to better capture the spatial distribution and gene expression diversity of the original dataset. Random sampling may not be sufficient to preserve important biological patterns in the data.
 indices = torch.randperm(N)[:M]
 #indices = torch.linspace(0, N - 1, M).long()
 X_hat = X_orig[indices].clone()
@@ -55,21 +81,26 @@ X_hat.requires_grad_(True)
 P_hat.requires_grad_(True)
 S_hat = (X_hat, P_hat)
 
+#%% Set optimization parameters
+# TODO: Try other optimizers LBFGS
 optimizer = optim.Adam([
     {'params': [X_hat], 'lr': 0.01},  
     {'params': [P_hat], 'lr': 0.005}    
     ])
 
+# TODO: Plot loss decreasing. Tune optimizer hyperparameters (learning rates, weight decay, etc.) to improve convergence and final results.
 epochs = 50
-sigma = 0.10
 
+#%% Optimization loop
 print("Starting optimization loop...")
+
+term0 = varifold_sp(S, S, bandwith_varifold)
 for epoch in range(epochs):
     optimizer.zero_grad()
     
-    term1 = varifold_sp(S_hat, S_hat, sigma)
-    term2 = varifold_sp(S, S_hat, sigma)
-    loss = term1 - 2 * term2 
+    term1 = varifold_sp(S_hat, S_hat, bandwith_varifold)
+    term2 = varifold_sp(S, S_hat, bandwith_varifold)
+    loss = term0 + term1 - 2 * term2
     
     loss.backward()
     optimizer.step()
@@ -93,8 +124,9 @@ p_hat_final = P_hat.detach().cpu().numpy()
 cloud_orig = pv.PolyData(x_orig_final)
 cloud_hat = pv.PolyData(x_hat_final)
 
-cloud_orig.point_data["Gene_0"] = P_np[:, 0]
-cloud_hat.point_data["Gene_0"] = p_hat_final[:, 0]
+# TODO: Fix export : export all feature and/or new summary feature for visualization to check quality: e.g.
+cloud_orig.point_data["Gene_weight"] = Feature_BarSeq.sum(dim=1)
+cloud_hat.point_data["Gene_weight"] = p_hat_final.sum(dim=1)
 
 cloud_orig.save("1.4-62k.vtp")
 cloud_hat.save("1.4-1k.vtp")
